5f062df2ae329c6f65ff0bb41991a6f9de4c69a7
shariqahmed123 <49516795+shariqahmed123@users.noreply.github.com>
2019-07-23 17:11:30 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function openSlideMenu(){
-    document.getElementById('menu').style.width = '250px';
-    document.getElementById('content').style.marginLeft = '250px';
-  }
-  function closeSlideMenu(){
-    document.getElementById('menu').style.width = '0';
-    document.getElementById('content').style.marginLeft = '0';
-  }
